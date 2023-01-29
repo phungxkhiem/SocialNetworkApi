a5ea4cb3fe4b248a25fadbbceebcc7e349bdd1e1
Khiem Thanh Phung <khiemphung@khiems-mbp.attlocal.net>
2023-01-29 14:39:36 -0800

    added index.js file.

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,4 +1,4 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const {
   getUsers,
   getSingleUser,
@@ -7,15 +7,12 @@ const {
   deleteUser,
   addFriend,
   removeFriend,
-} = require('../../controllers/user-controller');
+} = require("../../controllers/user-controller");
 
+router.route("/").get(getUsers).post(createUser);
 
-router.route('/').get(getUsers).post(createUser);
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
 
-
-router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
-
-
-router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
-const router = require("express").Router();
-const apiRoutes = require("./api");
+const router = require('express').Router()
+const apiRoutes = require('./api')
 
-router.use("/api", apiRoutes);
-router.use((req, res) => {
-  res.status(404).send("<h1>404 Error</h1>");
-});
+router.use('/api', apiRoutes)
+
 module.exports = router;
